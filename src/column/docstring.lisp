c409f3f33c1da91a572f51e76593d6e31a967da9
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2019-02-07 11:05:23 +0100

    Use true and false instead of nil and not-nil

--- src/column/docstring.lisp
+++ src/column/docstring.lisp
@@ -13,7 +13,7 @@
      :exceptional-situations "Should signal condition if this operation would move beyond column bounds."))
 
   (function replica
-    (:description "Creates new column with the same content as the old one. Changes in the new istance won't effect the COLUMN. Changes in the COLUMN may effect the new instance if ISOLATE is nil and will not effect new instance if ISOLATE is not-nil."))
+    (:description "Creates new column with the same content as the old one. Changes in the new istance won't effect the COLUMN. Changes in the COLUMN may effect the new instance if ISOLATE is false and will not effect new instance if ISOLATE is true."))
 
   (function column-at
     (:description "Obtains value under INDEX from the COLUMN."
