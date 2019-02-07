eeedd7d745a2972f570614861fe2d9ef5a234163
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2019-02-07 10:57:58 +0100

    Added iterator errors.

--- src/column/conditions.lisp
+++ src/column/conditions.lisp
@@ -5,6 +5,20 @@
   ())
 
 
-(define-condition index-out-of-bounds (column-error
-                                       cl-ds:argument-out-of-bounds)
+(define-condition iterator-error (program-error)
+  ())
+
+
+(define-condition index-out-of-column-bounds (column-error
+                                              cl-ds:argument-out-of-bounds)
+  ())
+
+
+(define-condition no-such-column (column-error
+                                  cl-ds:argument-out-of-bounds)
+  ())
+
+
+(define-condition iterator-reached-end (iterator-error
+                                        cl-ds:textual-error)
   ())
--- src/column/docstring.lisp
+++ src/column/docstring.lisp
@@ -15,11 +15,11 @@
   (function column-at
     (:description "Obtains value under INDEX from the COLUMN."
      :returns "Value under INDEX of the COLUMN. :NULL if location is empty."
-     :exceptional-situations "Will signal INDEX-OUT-OF-BOUNDS if index is out of column bounds."))
+     :exceptional-situations "Will signal INDEX-OUT-OF-COLUMN-BOUNDS if index is out of column bounds."))
 
   (function (setf column-at)
     (:description "Sets value under INDEX in the COLUMN."
-     :exceptional-situations "Will signal INDEX-OUT-OF-BOUNDS if index is out of column bounds."))
+     :exceptional-situations "Will signal INDEX-OUT-OF-COLUMN-BOUNDS if index is out of column bounds."))
 
   (function column-size
     (:description "The total size of the COLUMN.")))
