8964399498b3735990a912b690e6b34c450e7182
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2019-02-07 10:50:35 +0100

    Added conditions for out of bounds access.

--- src/column/conditions.lisp
+++ src/column/conditions.lisp
@@ -1,1 +1,10 @@
 (in-package #:cl-df.column)
+
+
+(define-condition column-error (program-error)
+  ())
+
+
+(define-condition index-out-of-bounds (column-error
+                                       cl-ds:argument-out-of-bounds)
+  ())
