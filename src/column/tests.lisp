14128f0bafcf360386b5fd687bcba5be71b5c0f6
Marek Kochanowicz <sirherrbatka@gmail.com>
2019-02-10 21:41:30 +0100

    Added extra test cases.

--- src/column/tests.lisp
+++ src/column/tests.lisp
@@ -1,6 +1,6 @@
 (in-package #:cl-df.column)
 
-(prove:plan 512)
+(prove:plan 768)
 
 (let* ((column (make-sparse-material-column))
        (iterator (make-iterator column)))
@@ -21,6 +21,20 @@
   (iterate
     (for j from 255 downto 0)
     (for i from 0 below 256)
+    (prove:is (column-at column i) j))
+  (setf iterator (make-iterator column))
+  (iterate
+    (for j from 128 downto 0)
+    (setf (iterator-at iterator 0) j)
+    (move-iterator iterator 1))
+  (finish-iterator iterator)
+  (iterate
+    (for i from 0 below 128)
+    (for j from 128 downto 0)
+    (prove:is (column-at column i) j))
+  (iterate
+    (for i from 128 below 256)
+    (for j from 127 downto 0)
     (prove:is (column-at column i) j)))
 
 (prove:finalize)
