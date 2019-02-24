19d9813ee37d8c4b8311c6e6ca3fb05ec0d276ea
Marek Kochanowicz <sirherrbatka@gmail.com>
2019-02-24 10:16:19 +0100

    Added proper setf generic functions.

--- src/header/protocol.lisp
+++ src/header/protocol.lisp
@@ -5,6 +5,10 @@
 
 (defgeneric index-to-alias (header index))
 
+(defgeneric (setf index-to-alias) (new-value header index))
+
 (defgeneric column-type (header column))
+
+(defgeneric (setf column-type) (header column))
 
 (defgeneric make-header (class &rest columns))
