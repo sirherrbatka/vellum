a8a7c0709e9319cea92c50750efdd37fa49d6b76
Marek Kochanowicz <sirherrbatka@gmail.com>
2019-03-01 20:11:42 +0100

    Remove destructive part of the protocol.

--- src/header/protocol.lisp
+++ src/header/protocol.lisp
@@ -5,10 +5,6 @@
 
 (defgeneric index-to-alias (header index))
 
-(defgeneric (setf index-to-alias) (new-value header index))
-
 (defgeneric column-type (header column))
-
-(defgeneric (setf column-type) (header column))
 
 (defgeneric make-header (class &rest columns))
