49fab584e9418ef86f713a4d971026d0b519edd8
Marek Kochanowicz <sirherrbatka@gmail.com>
2019-03-03 17:18:33 +0100

    Reformatted code.

--- src/csv/implementation.lisp
+++ src/csv/implementation.lisp
@@ -12,15 +12,16 @@
                                 (unquoted-empty-string-is-nil t)
                                 (trim-outer-whitespace t))
   (declare (ignore options))
-  (let ((result (~> input cl-ds.fs:line-by-line
-                    (cl-ds.alg:on-each (lambda (x)
-                                         (cl-csv:read-csv-row
-                                          x
-                                          :separator separator
-                                          :trim-outer-whitespace trim-outer-whitespace
-                                          :unquoted-empty-string-is-nil unquoted-empty-string-is-nil
-                                          :quoted-empty-string-is-nil quoted-empty-string-is-nil
-                                          :quote quote))))))
+  (let* ((fn (lambda (x)
+               (cl-csv:read-csv-row
+                x
+                :separator separator
+                :trim-outer-whitespace trim-outer-whitespace
+                :unquoted-empty-string-is-nil unquoted-empty-string-is-nil
+                :quoted-empty-string-is-nil quoted-empty-string-is-nil
+                :quote quote)))
+         (result (~> input cl-ds.fs:line-by-line
+                     (cl-ds.alg:on-each fn))))
     (when header
       (cl-ds:consume-front result))
     (make 'cl-df.header:forward-proxy-frame-range
