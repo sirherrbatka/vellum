9e3c34248656ddfa74b2739f5db9859d6a8a5483
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2019-03-05 08:36:50 +0100

    Support both input as pathname and range of files.

--- src/csv/implementation.lisp
+++ src/csv/implementation.lisp
@@ -1,8 +1,38 @@
 (in-package #:cl-df.csv)
 
 
+
 (defmethod cl-df:from-stream ((format (eql ':csv))
-                              input
+                              (input pathname)
+                              &rest options
+                              &key
+                                (separator #\,)
+                                (quote #\")
+                                (header t)
+                                (quoted-empty-string-is-nil nil)
+                                (unquoted-empty-string-is-nil t)
+                                (trim-outer-whitespace t))
+  (declare (ignore options))
+  (let ((frame-header (cl-df.header:header))
+        (fn (lambda (x)
+              (cl-csv:read-csv-row
+               x
+               :separator separator
+               :trim-outer-whitespace trim-outer-whitespace
+               :unquoted-empty-string-is-nil unquoted-empty-string-is-nil
+               :quoted-empty-string-is-nil quoted-empty-string-is-nil
+               :quote quote))))
+    (cl-ds.fs:with-file-ranges ((result (cl-ds.fs:line-by-line input)))
+      (when header
+        (cl-ds:consume-front result))
+      (cl-ds.fs:close-inner-stream result)
+      (make 'cl-df.header:forward-proxy-frame-range
+            :original-range (cl-ds.alg:on-each result fn)
+            :header frame-header))))
+
+
+(defmethod cl-df:from-stream ((format (eql ':csv))
+                              (input cl-ds:fundamental-forward-range)
                               &rest options
                               &key
                                 (separator #\,)
@@ -21,10 +51,16 @@
                 :unquoted-empty-string-is-nil unquoted-empty-string-is-nil
                 :quoted-empty-string-is-nil quoted-empty-string-is-nil
                 :quote quote)))
-         (result (~> input cl-ds.fs:line-by-line
-                     (cl-ds.alg:on-each fn))))
-    (when header
-      (cl-ds:consume-front result))
+         (result (~> input
+                     (cl-ds.alg:on-each
+                      (lambda (x)
+                        (cl-ds.fs:with-file-ranges
+                            ((inner (cl-ds.alg:on-each
+                                     x #'cl-ds.fs:line-by-line)))
+                          (when header
+                            (cl-ds:consume-front inner))
+                          (cl-ds.alg:on-each inner fn))))
+                     cl-ds.alg:chain-traversable)))
     (make 'cl-df.header:forward-proxy-frame-range
           :original-range result
           :header frame-header)))
