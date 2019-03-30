c48ffb8b515a8f61dc9103c3a9ee58e9045c5195
Marek Kochanowicz <sirherrbatka@gmail.com>
2019-03-30 13:17:21 +0100

    Allow passing multiple starts and ends to selection.

--- src/table/types.lisp
+++ src/table/types.lisp
@@ -35,13 +35,16 @@
           :reader read-ends)))
 
 
-(defun selection (start end)
+(defun selection (start end &rest more-start-ends)
   (declare (type non-negative-fixnum start end))
-  (let* ((arguments (~> `(,start ,end) (batches 2)))
+  (let* ((arguments (~> `(,start ,end ,@more-start-ends) (batches 2)))
          (starts (map '(vector fixnum) #'first arguments))
          (ends (map '(vector fixnum) #'second arguments)))
-    (assert (every (compose (curry #'eql 2) #'length)
-                   arguments))
+    (unless (every (compose (curry #'eql 2) #'length) arguments)
+      (error 'cl-ds:argument-error
+             :argument 'more-start-ends
+             :format-control "Odd number of arguments passed to the selection (~a)."
+             :format-arguments (length more-start-ends)))
     (make 'selection :starts starts :ends ends)))
 
 
