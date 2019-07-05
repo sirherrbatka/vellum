92c09645fff13bf91875c5f1d2256a52267ae0e9
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2019-07-05 11:56:40 +0200

    Added option to pass custom variable name to the body.

--- src/header/macros.lisp
+++ src/header/macros.lisp
@@ -9,24 +9,34 @@
 
 (defmacro body (selected-columns &body body)
   (let ((gensyms (mapcar (lambda (x) (declare (ignore x)) (gensym))
+                         selected-columns))
+        (names (mapcar (lambda (x)
+                         (econd
+                           ((symbolp x) x)
+                           ((listp x) (first x))))
+                       selected-columns))
+        (columns (mapcar (lambda (x)
+                           (econd
+                             ((symbolp x) x)
+                             ((listp x) (second x))))
                          selected-columns)))
     (with-gensyms (!arg)
       `(lambda (&rest ,!arg)
          (declare (ignore ,!arg))
-         (let* (,@(mapcar (lambda (binding)
-                            (assert (symbolp binding))
-                            `(,binding (rr ,binding)))
-                          selected-columns)
+         (let* (,@(mapcar (lambda (name column)
+                            `(,name (rr ,column)))
+                          names columns)
                 ,@(mapcar (lambda (binding gensym)
                             `(,binding ,gensym))
-                          selected-columns
+                          names
                           gensyms))
            (declare (special ,@selected-columns))
            ,@body
-           ,@(mapcar (lambda (binding gensym)
-                       `(unless (eql ,binding ,gensym)
-                          (setf (rr ,binding) ,gensym)))
-                     selected-columns
+           ,@(mapcar (lambda (column name gensym)
+                       `(unless (eql ,name ,gensym)
+                          (setf (rr ,column) ,gensym)))
+                     columns
+                     names
                      gensyms))))))
 
 
