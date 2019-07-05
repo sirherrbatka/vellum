9e24e14d625a7aed5cb783188334d3013d5ee48c
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2019-07-05 11:03:31 +0200

    Added special variables for columns in the in body macro.

--- src/header/macros.lisp
+++ src/header/macros.lisp
@@ -8,15 +8,26 @@
 
 
 (defmacro body (selected-columns &body body)
-  (with-gensyms (!arg)
-    `(lambda (&rest ,!arg)
-       (declare (ignore ,!arg))
-       (symbol-macrolet
-           ,(mapcar (lambda (binding)
-                      (assert (symbolp binding))
-                      `(,binding (rr ,binding)))
-             selected-columns)
-         ,@body))))
+  (let ((gensyms (mapcar (lambda (x) (declare (ignore x))
+                           (gensym)) selected-columns)))
+    (with-gensyms (!arg)
+      `(lambda (&rest ,!arg)
+         (declare (ignore ,!arg))
+         (let* (,@(mapcar (lambda (binding)
+                            (assert (symbolp binding))
+                            `(,binding (rr ,binding)))
+                          selected-columns)
+                ,@(mapcar (lambda (binding gensym)
+                            `(,binding ,gensym))
+                          selected-columns
+                          gensyms))
+           (declare (special ,@selected-columns))
+           ,@body
+           ,@(mapcar (lambda (binding gensym)
+                       `(unless (eql ,binding ,gensym)
+                          (setf (rr ,binding) ,gensym)))
+                     selected-columns
+                     gensyms))))))
 
 
 (defmacro brr (column)
