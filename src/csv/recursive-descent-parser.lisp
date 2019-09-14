37bcc78b94f567290dcceae3352c73da744b956b
Marek Kochanowicz <sirherrbatka@gmail.com>
2019-09-14 20:28:41 +0200

    corrected few errors in define-parser macro

--- src/csv/recursive-descent-parser.lisp
+++ src/csv/recursive-descent-parser.lisp
@@ -1,12 +1,12 @@
 (cl:in-package #:cl-df.csv)
 
 
-(defmacro define-parser (name (&body body))
+(defmacro define-parser (name &body body)
   `(defun ,name (separator escape-char skip-whitespace
                  quote line output path
                  line-index field-index result-index)
      (when (and (= field-index (length output))
-                (= line-index (lengh line)))
+                (= line-index (length line)))
        (return-from ,name t))
      (lret ((result
              (macrolet ((descent (into &key line-index field-index result-index)
@@ -15,10 +15,17 @@
                                   ,@(list (or line-index 'line-index)
                                           (or field-index 'field-index)
                                           (or result-index 'result-index)))))
-               (let ((char (aref line input-index)))
+               (let ((char (aref line line-index)))
                  ,@body))))
        (unless result
          (setf (fill-pointer (aref output field-index)) result-index)))))
+
+
+(define-parser parse-char
+  (switch (char)
+    (separator cl-ds.utils:todo)
+    (quote cl-ds.utils:todo)
+    ))
 
 
 (defun recursive-descent-parse-csv-line (separator escape-char skip-whitespace
