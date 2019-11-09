1347fb4904d9ce2a9f16fd6c737b0df6564d1625
Marek Kochanowicz <sirherrbatka@gmail.com>
2019-11-09 22:33:17 +0100

    Just add sparse spacing between elements.

--- src/integration/clim.lisp
+++ src/integration/clim.lisp
@@ -3,28 +3,26 @@
 
 (defun show-data-frame (clim-stream data-frame)
   (cl-df:with-table (data-frame)
-    (formatting-table (clim-stream)
-      (formatting-row (clim-stream)
-        (surrounding-output-with-border
-            (clim-stream :background +grey+
-                         :padding-top 0
-                         :padding-bottom 0)
-          (iter:iterate
-            (iter:with header = (cl-df:header))
-            (iter:for i from 0 below (cl-df:column-count data-frame))
-            (iter:for alias = (or (ignore-errors (cl-df.header:index-to-alias header i))
-                             i))
+    (formatting-table (clim-stream
+                       :multiple-columns t
+                       :y-spacing 20
+                       :x-spacing 20)
+      (iter:iterate
+        (iter:with header = (cl-df:header))
+        (iter:for i from 0 below (cl-df:column-count data-frame))
+        (iter:for alias = (or (ignore-errors (cl-df.header:index-to-alias header i))
+                              i))
+        (formatting-column (clim-stream)
+          (surrounding-output-with-border (clim-stream
+                                           :shape :underline)
             (formatting-cell (clim-stream)
-              (princ alias clim-stream)))))
-      (cl-df:transform data-frame
-                       (lambda (&rest ignore)
-                         (declare (ignore ignore))
-                         (formatting-row (clim-stream)
-                           (iter:iterate
-                             (iter:for i from 0 below (cl-df:column-count data-frame))
+              (princ alias clim-stream)))
+          (cl-df:transform data-frame
+                           (lambda (&rest ignore)
+                             (declare (ignore ignore))
                              (formatting-cell (clim-stream)
-                               (print (cl-df:rr i) clim-stream)))))
-                       :in-place t))))
+                               (print (cl-df:rr i) clim-stream)))
+                           :in-place t))))))
 
 
 (define-application-frame table-view ()
