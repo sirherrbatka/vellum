af5195fd735fddbdd376a322561cf76eb1db4c0a
Marek Kochanowicz <sirherrbatka@gmail.com>
2020-03-20 21:55:52 +0100

    Reformatted code.

--- src/api/functions.lisp
+++ src/api/functions.lisp
@@ -182,9 +182,7 @@
                      (for j from 1 below length)
                      (setf (aref data j) (make-array (aref lengths j)
                                                      :initial-element :null)))
-                   (setf (aref data 0) (~> input
-                                           (aref 0)
-                                           (aref i)))
+                   (setf (aref data 0) (~> input (aref 0) (aref i)))
                    (setf (aref result i) data)
                    (finally (return (cl-ds:whole-range result)))))
                 (t (cartesian-product input)))))
