75e3990d27262847be50589e7803e4f50e692ced
Cyrus Harmon <cyrus@bobobeach.com>
2020-12-30 11:09:02 -0800

    remove plotly-format-pick

--- src/plot/internal.lisp
+++ src/plot/internal.lisp
@@ -98,11 +98,6 @@
         t)))
 
 
-(defun plotly-format-pick (field-a value-a field-b value-b)
-  (or (plotly-format-no-nulls field-a value-a)
-      (plotly-format-no-nulls field-b value-b)))
-
-
 (defun plotly-generate-data/impl (stack geometrics index.table)
   (bind ((mapping (read-mapping geometrics))
          (data (data-layer stack))
