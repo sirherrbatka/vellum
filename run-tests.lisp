c256d4b99fa462bbfe8f2ddbe211bf44dce47b31
Marek Kochanowicz <sirherrbatka@gmail.com>
2021-03-21 20:00:55 +0100

    quickload tests silently in run-tests.

--- run-tests.lisp
+++ run-tests.lisp
@@ -3,7 +3,7 @@
   (when (probe-file quicklisp-init)
     (load quicklisp-init)))
 
-(quicklisp:quickload :vellum-tests)
+(quicklisp:quickload :vellum-tests :silent t)
 
 (setf prove:*enable-colors* nil)
 
