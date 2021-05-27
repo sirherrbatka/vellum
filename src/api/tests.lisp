4f57899008d816d51e819a5b043ce647c977b8e6
Marek Kochanowicz <sirherrbatka@gmail.com>
2021-05-27 10:42:13 +0200

    Added test case.

--- src/api/tests.lisp
+++ src/api/tests.lisp
@@ -1,6 +1,6 @@
 (cl:in-package #:vellum)
 
-(prove:plan 2)
+(prove:plan 4)
 
 (let* ((frame-1 (transform (make-table :columns '(a b))
                            (vellum:bind-row (a b)
@@ -15,8 +15,23 @@
        (result (join :hash :inner
                      `((:frame-1 ,frame-1 a)
                        (:frame-2 ,frame-2 a)))))
-  (vellum:show :text result)
   (prove:is (column-count result) 4)
   (prove:is (row-count result) 5))
+
+(let* ((frame-1 (transform (make-table :columns '(a b))
+                  (vellum:bind-row (a b)
+                    (setf a vellum.table:*current-row*)
+                    (setf b (mod a 2)))
+                  :end 5))
+       (frame-2 (transform (make-table :columns '(a b))
+                  (vellum:bind-row (a b)
+                    (setf a vellum.table:*current-row*)
+                    (setf b (mod a 2)))
+                  :end 5))
+       (result (join :hash :inner
+                     `((:frame-1 ,frame-1 b)
+                       (:frame-2 ,frame-2 b)))))
+  (prove:is (column-count result) 4)
+  (prove:is (row-count result) 13))
 
 (prove:finalize)
