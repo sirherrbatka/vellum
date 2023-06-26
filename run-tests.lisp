986ee36ca80b83da946723e8579cd08c45b35e1e
Marek Kochanowicz <sirherrbatka@gmail.com>
2023-06-26 16:56:52 +0200

    Remove unwind-protect.

--- run-tests.lisp
+++ run-tests.lisp
@@ -5,16 +5,15 @@
 
 (quicklisp:quickload :vellum-tests :silent t)
 
-(unwind-protect
-     (let ((*error-output* (make-broadcast-stream))
-           (prove:*test-result-output* *standard-output*)
-           (*standard-output* (make-broadcast-stream))
-           (prove:*enable-colors* t)
-           (prove:*default-reporter* :dot))
-       (handler-bind
-           ((lparallel.kernel:no-kernel-error
-              (lambda (c)
-                (declare (ignore c))
-                (invoke-restart 'lparallel.kernel:make-kernel 4))))
-         (prove:run :vellum-tests)))
-  (cl-user::quit))
+(let ((*error-output* (make-broadcast-stream))
+      (prove:*test-result-output* *standard-output*)
+      (*standard-output* (make-broadcast-stream))
+      (prove:*enable-colors* t)
+      (prove:*default-reporter* :dot))
+  (handler-bind
+      ((lparallel.kernel:no-kernel-error
+         (lambda (c) (declare (ignore c))
+           (invoke-restart 'lparallel.kernel:make-kernel 4))))
+    (prove:run :vellum-tests)))
+
+(cl-user:quit)
