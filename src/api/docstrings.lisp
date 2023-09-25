0b6abdc1d2dc7fa009b9919f671054586ddb3b6a
Marek Kochanowicz <sirherrbatka@gmail.com>
2023-09-25 15:17:47 +0200

    Added more info to docstrings

--- src/api/docstrings.lisp
+++ src/api/docstrings.lisp
@@ -10,6 +10,12 @@
                         "(prove:is (vellum:find-row (vellum:to-table '((1 2 3) (3 4 5)) :columns '(a b c)) (vellum:bind-row (c) (when (= c 5) (vellum:found-row a)))) '(3))")
              :see-also (found-row)))
 
+  (function column-list
+            (:description "Returns selected column values in the current row as a list."))
+
+  (function rename-columns
+            (:description "Creates new frame from the old one with altered column names."))
+
   (macro found-row
          (:description "Evaluation of this macro expansion will inform find-row that row was found"
           :see-also (find-row))))
