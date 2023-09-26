dfbd141b467d91cb2bdc40b8dd8ad2888ae12ce8
Marek Kochanowicz <sirherrbatka@gmail.com>
2023-09-26 13:09:53 +0200

    Added keys to frame-specs JOIN function.

--- src/api/docstrings.lisp
+++ src/api/docstrings.lisp
@@ -13,12 +13,29 @@
   (function add-columns
             (:description "Creates new frame from the old one by adding additional columns."))
 
+  (function join
+            (:description "Performs METHOD (:LEFT or :INNER) join using ALGORITHM (:HASH) on FRAME-SPECS list. Each element of FRAME-SPECS list contains elements in the following order label for data-frame, data-frame and description of columns used as join key. Each description is either name of the column, number of the column; or two elements list consisting of name or number of the column and function designator used as key."
+             :examples "(let* ((frame-1 (transform (vellum:make-table :columns '(a b))
+                           (vellum:bind-row (a b)
+                             (setf a vellum.table:*current-row*)
+                             (setf b (format nil \"a~a\" a)))
+                             :end 5))
+         (frame-2 (transform (vellum:make-table :columns '(a b))
+                             (vellum:bind-row (a b)
+                               (setf a vellum.table:*current-row*)
+                               (setf b (format nil \"b~a\" a)))
+                             :end 5))
+         (result (vellum:join :hash :inner
+                              `((:frame-1 ,frame-1 a)
+                                (:frame-2 ,frame-2 a)))))
+    (vellum:show :text result))"))
+
   (function column-list
             (:description "Returns selected column values in the current row as a list."))
 
   (function rename-columns
             (:description "Creates new frame from the old one with altered column names."))
 
-  (macro found-row
+  (function found-row
          (:description "Evaluation of this macro expansion will inform find-row that row was found"
           :see-also (find-row))))
